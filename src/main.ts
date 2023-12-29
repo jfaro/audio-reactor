ea534c01c5f43e6d07ffda890e6012fa257b2491
Julien Faro <jnfaro99@gmail.com>
2023-12-29 17:00:13 -0500

    Create app module

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,3 @@
-import AudioManager from "./AudioManager"
-import AudioVisualizer from "./AudioVisualizer";
-
-export default class App {
-  static visualizer: AudioVisualizer;
-  static audioManager: AudioManager;
-
-  constructor() {
-    App.audioManager = new AudioManager();
-
-    // Starting update loop requires audio manager to be initialized. Maybe a 
-    // reference to existing audio source should be required as an argument.
-    App.visualizer = new AudioVisualizer();
-    App.visualizer.update()
-
-    // Register click handler
-    document.addEventListener('click', this.onClick)
-  }
-
-  async onClick() {
-    if (!App.audioManager.isPlaying) {
-      await App.audioManager.play()
-    } else {
-      App.audioManager.pause()
-    }
-  }
-}
+import App from "./App";
 
 new App();
